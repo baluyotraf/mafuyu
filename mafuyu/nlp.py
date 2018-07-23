e0a958330be7ea0340d7e9b9ed20981bc5acc3b1
baluyotraf <baluyotraf@outlook.com>
2018-07-23 08:19:02 +0800

    Added class helper for creating and loading character embedding

--- mafuyu/nlp.py
+++ mafuyu/nlp.py
@@ -66,6 +66,75 @@ class GloveEmbedding:
         return '<UNK>'
 
 
+class CharacterEmbedding:
+
+    def __init__(self):
+        self._char_count = 0
+        self._char_to_index = {}
+        self._index_to_char = []
+        self._unk_id = 0
+
+    def _initialize_from_char_list(self, char_list):
+        char_set = set(char_list)
+
+        self._unk_id = len(char_set)
+        self._char_count = self._unk_id + 1
+
+        self._index_to_char = [c for c in char_set]
+        self._char_to_index = dict(zip(self._index_to_char, range(self._unk_id)))
+
+    def _initialize_from_sentences(self, sentences):
+        char_list = [c for s in sentences for c in s]
+        self._initialize_from_char_list(char_list)
+
+    def char_to_index(self, char):
+        return self._char_to_index.get(char, self.unk_id)
+
+    def index_to_char(self, index):
+        if index < 0:
+            raise IndexError
+        else:
+            try:
+                return self._index_to_char[index]
+            except IndexError:
+                return self.unk
+
+    @property
+    def char_count(self):
+        return self._char_count
+
+    @property
+    def unk(self):
+        return '<UNK>'
+
+    @property
+    def unk_id(self):
+        return self._unk_id
+
+    def save(self, path):
+        with open(path, 'w') as file:
+            for c in self._index_to_char:
+                file.write(c + '\n')
+
+    @classmethod
+    def from_sentences(cls, sentences):
+        instance = cls()
+        instance._initialize_from_sentences(sentences)
+        return instance
+
+    @classmethod
+    def from_char_list(cls, char_list):
+        instance = cls()
+        instance._initialize_from_char_list(char_list)
+        return instance
+
+    @classmethod
+    def load(cls, path):
+        with open(path, 'r') as file:
+            char_list = [f.strip() for f in file]
+        return cls.from_char_list(char_list)
+
+
 def print_length_distribution(data, rows=3, plot_size=(10, 2)):
     lengths = [len(d) for d in data]
     print_distribution(lengths, 'Lengths', rows, plot_size)
